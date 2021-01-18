b9d8b1a90ee85408f8e8a8c9432e0657f17b4597
SenorDollar <salaahcode@gmail.com>
2021-01-18 12:31:25 +0330

    First Update

---
+++ Buki/Source/Buki/Buki.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Buki.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Buki, "Buki" );
---
+++ Buki/Source/Buki/BukiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BukiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUKI_API ABukiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
